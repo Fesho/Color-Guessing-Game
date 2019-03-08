0aab37f1e05c2ce60228affbf5d803089f5d6665
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-03-08 17:58:46 -0500

    compare between cliked and icked color

--- colorguess.js
+++ colorguess.js
@@ -8,3 +8,32 @@ var colors = [
 	"rgb(255, 0, 255)"
 
 ]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[4];
+var colorDisplay = document.getElementById('discolor');
+
+colorDisplay.textContent= pickedColor;
+
+for (var i =0;  i< squares.length; i++) {
+	// add initial colors to each square
+	squares[i].style.background=colors[i];
+	//add listeners to the square
+
+
+
+squares[i].addEventListener("click", function(){
+	// determine the color rgb on each clicked square
+var clickedColor = this.style.background;
+// compare clicked color with picked color
+if (clickedColor === pickedColor)
+{
+	alert("Correct!");
+}else
+	{alert ("wrong");}
+
+	});
+}
+
+
+
